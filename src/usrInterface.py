a0c53d47d4224650848196f7765b362e74813f90
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-04-24 12:31:53 -0700

    Added runApp function with menu options

--- src/usrInterface.py
+++ src/usrInterface.py
@@ -16,6 +16,37 @@ def start():
     print("========================================")
     print()
     print("Please login with valid email to continue")
+
+    # Call login() to get userName
     userName = login.login()
 
-    #Run the app after successful login
+    # Call runApp with userName as param
+    runApp(userName)
+
+def runApp(userName):
+    """
+    This function welcomes the user by email, gives options to view seating, purchase tickets,
+    search by email, display all purchases or quit
+    """
+
+    # Store userName for purchase info
+    user = userName
+
+    print("========================================")
+    print("Welcome " + user)
+    print()
+    print("Please select from the following options")
+    print("[V] View Seating")
+    print("[B] Buy tickets")
+    print("[S] Search by email")
+    print("[D] Display all purchases")
+    print("[Q] Quit")
+    print("========================================")
+    
+    userOption = input("Please enter a command: ")
+    lowerOption = userOption.lower()
+    firstChar = lowerOption[0:1]
+
+
+
+   
