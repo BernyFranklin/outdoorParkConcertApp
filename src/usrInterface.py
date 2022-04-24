b81e2eb44245ed935d91bad918a7be564a174249
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-04-24 11:15:38 -0700

    Set up JSON for seating chart, created main.py and usrInterface.py

---
+++ src/usrInterface.py
@@ -0,0 +1,18 @@
+import io
+
+"""
+    Module: userInterface
+    Description: This module presents a welcome prompt, 
+                 login prompt to store user data purchase, 
+                and interface to navigate purchases.
+"""
+
+def start():
+    print("========================================")
+    print("  Welcome to FrankFest Music Festival   ")
+    print("========================================")
+    print()
+    print("Please login with valid email to continue")
+
+    #Prompt user login here
+    #Run the app after successful login
