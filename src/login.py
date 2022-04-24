7b17b64ac88137db0fba8f6b0cae5472081bba05
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-04-24 12:02:01 -0700

    Added main.py, added login.py
    Login uses regex to validate email.

---
+++ src/login.py
@@ -0,0 +1,38 @@
+import io
+import getpass
+import re
+
+"""
+    Module: login
+    Description: This module lets the user login using a valid email address,
+                 verified using regex and submitting a password
+"""
+
+
+def login():
+    # Prime loop
+    emailValid = False
+    # Start loop for email validation
+    while (emailValid == False):
+        # Prompt user for emailAddress
+        emailAddress = input("Email: ")
+        emailValid = isValid(emailAddress)
+    
+    password = getpass.getpass("Password: ")
+
+
+    return emailAddress
+
+def isValid(email):
+    # Create regex for valid email
+    regex = re.compile(r'([A-Za-z0-9\._-]+)@([A-Za-z0-9\._-])+(\.[A-Za-z]{2,})+')
+
+    # Check for validity
+    if re.fullmatch(regex, email):
+        return True
+    else:
+        print("Invalid Email, please try again")
+        return False
+
+
+login()
---
+++ src/main.py
@@ -0,0 +1,9 @@
+import io
+import usrInterface as UI
+
+"""
+    Module: Main
+    Description: This is the main program that executes the app.
+"""
+
+UI.start()
